eaf799d1b56df787b06a0cfe530a0516f65e1898
andreahuncikova <andhun01@easv365.dk>
2024-02-26 10:36:48 +0100

    fixed stuff
    foreach & filter

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const myArray = ["apple", "banana", "cherry", "grape"];
+// const myArray = ["apple", "banana", "cherry", "grape"];
 
 
 // 1. using forEach with an array, it is like a loop
@@ -6,4 +6,59 @@ const myArray = ["apple", "banana", "cherry", "grape"];
 //     console.log(item)
 // })
 
-// 
+const products = [
+    {
+        title: 'Product 1', 
+        description: 'Description for product 1',
+        image: 'https://picsum.photos/200/150',
+        price: 19.99,
+        category: 'Electronics',
+    },
+    {
+        title: 'Product 2', 
+        description: 'Description for product 2',
+        image: 'https://picsum.photos/200/150',
+        price: 9.99,
+        category: 'Shoes',
+    },
+    {
+        title: 'Product 3', 
+        description: 'Description for product 3',
+        image: 'https://picsum.photos/200/150',
+        price: 29.99,
+        category: 'Shoes',
+    }
+];
+
+// products.forEach(product => {
+//     console.log(`Title: ${product.title}`);
+// })
+
+const filteredProducts = products.filter(product => product.category == "Shoes")
+console.log(filteredProducts)
+
+const createCard = product => {
+    return`
+        <div class="card">
+        <h4>${product.title}</h4>
+        <h2>${product.description}</h2>
+        <img src="${product.image}" />
+        <p>Price: ${product.price}</p>
+        <p>Category: ${product.category}</p>
+        </div>
+    `;
+}
+
+products.forEach(product => {
+    console.log(createCard(product));
+})
+
+const productContainer = document.querySelector('.row')
+
+// products.forEach(product => {
+//     productContainer.innerHTML += createCard(product);
+// })
+
+filteredProducts.forEach(product => {
+    productContainer.innerHTML += createCard(product);
+})
