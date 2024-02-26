1d279999af4fa589314dfa280d2abdb82d3806c0
andreahuncikova <andhun01@easv365.dk>
2024-02-26 09:34:23 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const myArray = ["apple", "banana", "cherry", "grape"];
+
+
+// 1. using forEach with an array, it is like a loop
+// myArray.forEach(item => {
+//     console.log(item)
+// })
+
+// 
